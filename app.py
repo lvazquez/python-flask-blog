73197d733827e6473cd5b357b7dcca0d458e6493
lvazquez <luar.vazquez@gmail.com>
2025-05-13 22:57:35 -0300

    Primer commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+from sqlite3 import connect, Row, Connection
+
+conn = None
+def get_db_connection() -> Connection:
+    conn = connect('database.db')
+    conn.row_factory = Row
+    return conn
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template('index.html', posts=posts)
+
+@app.route('/hello')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.run()
